776eb016134396292a02a26c07020a4ef6f5712c
mai <maidiabbb6@gmail.com>
2025-06-30 14:33:28 +0300

    merge from onboarding

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,7 @@ import 'package:flutter_advanced_course/core/routing/app_router.dart';
 import 'package:flutter_advanced_course/doc_app.dart';
 
 void main() {
-  runApp(DocApp(appRouter: AppRouter(),));
+  runApp(DocApp(
+    appRouter: AppRouter(),
+  ));
 }
