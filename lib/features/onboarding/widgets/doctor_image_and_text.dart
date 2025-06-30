8fcf846225bb26e24a6f3f14c6b9d7e242182694
mai <maidiabbb6@gmail.com>
2025-06-30 15:26:25 +0300

    adding splash screen

--- lib/features/onboarding/widgets/doctor_image_and_text.dart
+++ lib/features/onboarding/widgets/doctor_image_and_text.dart
@@ -9,31 +9,29 @@ class DoctorImageAndText extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        SvgPicture.asset('assets/svgs/docdoc_logg_low_capacity.svg')
-      ,Container(
-        foregroundDecoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Colors.white,
-              Colors.white.withOpacity(0.0),
-            ],
-            begin: Alignment.bottomCenter,
-            end: Alignment.topCenter,
-            stops: const[0.14,0.4])
+        SvgPicture.asset('assets/svgs/docdoc_logg_low_capacity.svg'),
+        Container(
+          foregroundDecoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [
+                    Colors.white,
+                    Colors.white.withOpacity(0.0),
+                  ],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  stops: const [0.14, 0.4])),
+          child: Image.asset('assets/images/onboarding_doctor_image.png'),
         ),
-        child: Image.asset('assets/images/onboarding_doctor_image.png'),
-      ),
-      Positioned(
-        bottom: 30,
-        left: 0,
-        right: 0,
-        child: Text('Best Doctor\nAppointment App',
-        textAlign: TextAlign.center,
-        style: TextStyles.font32BlueBold.copyWith(
-          height: 1.4
-        ),)
-    )
-    ],
+        Positioned(
+            bottom: 30,
+            left: 0,
+            right: 0,
+            child: Text(
+              'Best Doctor\nAppointment App',
+              textAlign: TextAlign.center,
+              style: TextStyles.font32BlueBold.copyWith(height: 1.4),
+            ))
+      ],
     );
   }
 }
