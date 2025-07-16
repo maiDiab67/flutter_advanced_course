dfdc5f837d4f46fe143cc34814754009fbf26691
mai <maidiabbb6@gmail.com>
2025-07-16 07:05:06 +0300

    -added home blue image and container

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_course/features/home/ui/widgets/home_top_bar.dart';
 
+import 'widgets/doctors_blue_container.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -14,7 +16,7 @@ class HomeScreen extends StatelessWidget {
           margin: EdgeInsets.fromLTRB(20.0, 16.0, 20.0, 28.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: [HomeTopBar()],
+            children: [HomeTopBar(), DoctorsBlueContainer()],
           ),
         ),
       ),
